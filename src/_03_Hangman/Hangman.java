21732b57e9da1b823b3a0937f18507ae69c7937b
league <maddoxseul@sbsd.net>
2025-04-25 18:30:54 -0700

    finish stringbuilder

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,18 +1,36 @@
 package _03_Hangman;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Hangman {
-public static void main(String[] args) {
+public class Hangman implements KeyListener {
+	String mysteryWord;
 	Stack<String> words = new Stack();
 	Utilities utilites = new Utilities();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+public static void main(String[] args) {
+	Hangman hangman = new Hangman();
+	hangman.run();
+	}
+
+private void run() {
+	// TODO Auto-generated method stub
+	frame.add(panel);
+	panel.add(label);
+	frame.setSize(500,500);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.addKeyListener(this);
 	String number = JOptionPane.showInputDialog("Choose a number between 0 and 266");
 	int numbr = Integer.parseInt(number);
 	
@@ -27,5 +45,50 @@ public static void main(String[] args) {
 			
 		}
 	}
+	mysteryWord = words.pop();
+	int charCount = mysteryWord.length();
+	for(int i =0; i < charCount; i++) {
+		label.setText(label.getText()+"_");
 	}
+}
+
+@Override
+public void keyPressed(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyReleased(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyTyped(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	char key = arg0.getKeyChar();
+	if(mysteryWord.contains(String.valueOf(arg0.getKeyChar()))) {
+		ArrayList<Integer> locations = new ArrayList(); 
+		int start = 0;
+		while(true) {
+			int loc = mysteryWord.indexOf(key,start);
+			if(loc != -1) {
+				locations.add(loc);
+				start = loc+1;
+				if(start > mysteryWord.length()-1) {
+					break;
+				}
+			}
+			else {
+				break;
+			}
+		}
+		StringBuilder stringbuilder = new StringBuilder(label.getText());
+		for(int i = 0; i<locations.size();i++) {
+			stringbuilder.replace(locations.get(i), locations.get(i), "key");
+		}
+		//	label.setText();
+	}
+}
 }
