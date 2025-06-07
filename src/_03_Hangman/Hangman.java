cb1c3b0bed5e9ed78a274891868d090cc48682f4
Aarit Jayaswal <aaritrj@gmail.com>
2025-06-06 18:28:00 -0700

    figure out the formating on step 2

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -18,77 +18,87 @@ public class Hangman implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
-public static void main(String[] args) {
-	Hangman hangman = new Hangman();
-	hangman.run();
+	JLabel label2 = new JLabel();
+	int lives = 10;
+	public static void main(String[] args) {
+		Hangman hangman = new Hangman();
+		hangman.run();
 	}
 
-private void run() {
-	// TODO Auto-generated method stub
-	frame.add(panel);
-	panel.add(label);
-	frame.setSize(500,500);
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.addKeyListener(this);
-	String number = JOptionPane.showInputDialog("Choose a number between 0 and 266");
-	int numbr = Integer.parseInt(number);
-	
-	//for loop that has all of the below:
-	for(int i = 0; i < numbr; i++) {
-		String randomword = utilites.readRandomLineFromFile("dictionary.txt");
-		words.push(randomword);
-		if(words.contains(randomword)) {
-			words.remove(i);
-			randomword = utilites.readRandomLineFromFile("dictionary.txt");
+	private void run() {
+		// TODO Auto-generated method stub
+		frame.add(panel);
+		panel.add(label);
+		frame.add(label2);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
+		String number = JOptionPane.showInputDialog("Choose a number between 0 and 266");
+		int numbr = Integer.parseInt(number);
+
+		// for loop that has all of the below:
+		for (int i = 0; i < numbr; i++) {
+			String randomword = utilites.readRandomLineFromFile("dictionary.txt");
 			words.push(randomword);
-			
+			if (words.contains(randomword)) {
+				words.remove(i);
+				randomword = utilites.readRandomLineFromFile("dictionary.txt");
+				words.push(randomword);
+
+			}
 		}
+		mysteryWord = words.pop();
+		System.out.println(mysteryWord);
+		int charCount = mysteryWord.length();
+		for (int i = 0; i < charCount; i++) {
+			label.setText(label.getText() + "_");
+		}
+		label2.setText("Lives: " + lives);
 	}
-	mysteryWord = words.pop();
-	int charCount = mysteryWord.length();
-	for(int i =0; i < charCount; i++) {
-		label.setText(label.getText()+"_");
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
-}
 
-@Override
-public void keyPressed(KeyEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 
-@Override
-public void keyReleased(KeyEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	}
 
-@Override
-public void keyTyped(KeyEvent arg0) {
-	// TODO Auto-generated method stub
-	char key = arg0.getKeyChar();
-	if(mysteryWord.contains(String.valueOf(arg0.getKeyChar()))) {
-		ArrayList<Integer> locations = new ArrayList(); 
-		int start = 0;
-		while(true) {
-			int loc = mysteryWord.indexOf(key,start);
-			if(loc != -1) {
-				locations.add(loc);
-				start = loc+1;
-				if(start > mysteryWord.length()-1) {
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		char key = arg0.getKeyChar();
+		if (mysteryWord.contains(String.valueOf(key))) {
+			ArrayList<Integer> locations = new ArrayList();
+			int start = 0;
+			while (true) {
+				int loc = mysteryWord.indexOf(key, start);
+				if (loc != -1) {
+					locations.add(loc);
+					start = loc + 1;
+					if (start > mysteryWord.length() - 1) {
+						break;
+					}
+				} else {
 					break;
 				}
 			}
-			else {
-				break;
+
+			StringBuilder stringbuilder = new StringBuilder(label.getText());
+			for (int i = 0; i < locations.size(); i++) {
+				if(locations.get(i) != key) {
+					lives--;
+					label2.setText("Lives :" + lives);
+				}
+				String keyAsString = Character.toString(key);
+				stringbuilder.replace(locations.get(i), locations.get(i)+1, keyAsString);
 			}
+			label.setText(stringbuilder.toString());
 		}
-		StringBuilder stringbuilder = new StringBuilder(label.getText());
-		for(int i = 0; i<locations.size();i++) {
-			stringbuilder.replace(locations.get(i), locations.get(i), "key");
-		}
-		//	label.setText();
 	}
-}
 }
